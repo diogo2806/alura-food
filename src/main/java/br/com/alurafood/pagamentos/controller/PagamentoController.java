cbf4f38e93eba41ef899a96c3ccd4b863ab6d988
diogo2806 <diogo_2806@hotmail.com>
2023-06-24 22:25:26 +0000

    controller
    Signed-off-by: diogo2806 <diogo_2806@hotmail.com>

--- src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
+++ src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,5 +53,11 @@ public class PagamentoController {
 
         PagamentoDTO atualizado = service.atualizarPagamento(id, dto);
         return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<PagamentoDTO> remover (@PathVariable @NotNull Long id){
+        service.excluirPagamento(id);
+        return ResponseEntity.noContent().build();
     }
 }
