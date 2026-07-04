8358ccb21b28317a630dc219a93d483cf9fd3d00
Diogo Santos <diogo_2806@hotmail.com>
2026-07-04 08:58:01 -0300

    Adiciona javadoc do pacote controller

---
+++ src/main/java/br/com/alurafood/pagamentos/controller/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Camada responsavel por receber e responder as solicitacoes do processo de pagamentos.
+ *
+ * <p>Na visao de negocio, este pacote concentra as portas de entrada usadas por pessoas,
+ * telas ou outros sistemas para consultar, criar, atualizar e remover pagamentos. As
+ * classes deste pacote nao representam a regra completa do negocio; elas organizam a
+ * comunicacao inicial, validam a chegada dos dados e encaminham o fluxo para os servicos
+ * responsaveis por executar cada etapa.</p>
+ */
+package br.com.alurafood.pagamentos.controller;
